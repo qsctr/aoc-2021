f032c436e6f88b4765f940c0d4ce3d795630e8d9
Bretton <qsctr@users.noreply.github.com>
2021-12-19 10:47:27 +0800

    Add day 12 solutions

---
+++ day12/Day12.hs
@@ -0,0 +1,29 @@
+import           Data.Char
+import qualified Data.Map.Strict as M
+import qualified Data.Set        as S
+
+pathsAllSmallOnce conns = visit S.empty "start"
+  where visit _ "end" = 1
+        visit vs x
+            | isLower (head x) = if S.member x vs then 0 else go $ S.insert x vs
+            | otherwise = go vs
+          where go vs' = sum $ map (visit vs') $ cs M.! x
+        cs = foldr addConn M.empty conns
+        addConn (x, y) m = M.insertWith (++) x [y] $ M.insertWith (++) y [x] m
+
+pathsSingleSmallTwice conns = visit S.empty False "start"
+  where visit _ _ "end" = 1
+        visit vs smallTwice x
+            | isLower (head x) = if S.member x vs
+                then if smallTwice || x == "start" then 0 else goSmall True
+                else goSmall smallTwice
+            | otherwise = go vs smallTwice
+          where goSmall = go $ S.insert x vs
+                go vs' st' = sum $ map (visit vs' st') $ cs M.! x
+        cs = foldr addConn M.empty conns
+        addConn (x, y) m = M.insertWith (++) x [y] $ M.insertWith (++) y [x] m
+
+main = do
+    conns <- map (fmap tail . break (== '-')) . lines <$> readFile "input.txt"
+    print $ pathsAllSmallOnce conns
+    print $ pathsSingleSmallTwice conns
